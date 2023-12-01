a28bc9ac0544f825710034cd42052cd00f2bc291
Douglas Moreira <dougmoreira@Douglass-MBP.lan>
2023-12-01 13:50:06 -0500

    ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { QRCodeSVG } from 'qrcode.react'
 import { base64StringToBlob } from 'blob-util';
 import touchIcon from "../assets/touch_icon.png"
 import press from"../assets/press-to-start.png"
-import takePhotoIcon from"../assets/button.png"
 import countdown from '../assets/giphy_countdown.gif'
 
 
@@ -16,17 +14,6 @@ function Home() {
   const [data, setData]  =  useState(false)
 
   const getVideo = () =>{
-    // let width;
-    // let height;
-
-    // if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    //   document.write("mobile");
-    //   width =  414  
-    //   height = 238
-    // }else{
-    //   width =  238  
-    //   height = 414
-    // }
     navigator.mediaDevices
       .getUserMedia({ video:{width: 481, height:320}}).then( stream =>{
         let video =  videoRef.current;
@@ -66,41 +53,17 @@ function Home() {
   }
 
   function photoCountdown() {
+    // RUN COUNTDOWN AND TAKE THE PICTURE
     setStep(2)
     const myTimeout = setTimeout(takePhoto,2500);
     restart()
   }
 
-  // const generateDownloadLink = (b) =>{
-  //   let file = URL.createObjectURL(b)
-  //   let a  = document.getElementById('link')
-  //   a.download =  'Mean_Girls_2024.png'
-  //   a.href = file;
-  //   const canvas = document.querySelector('canvas');
-  //    const pngUrl = canvas
-  //      .toDataURL("image/png")
-  //      .replace("image/png", "image/octet-stream");
-  // }
 
   const postPicture =  (b64) =>{
-    // PUBLIC_READ_ONLY_TOKEN = 's2tbYsgFUTyOz4NluIu8OupD9leOqZMWvP31Veat'
-    // PUBLIC_EVENT_ID        = '107551'
-    // PUBLIC_PHOTOSTREAM_ID  = '64731' Mean Girls  
-    // PUBLIC_FULL_URL        = 'https://2b424657.goodburger-attendee.pages.dev'
-    // PUBLIC_API_BASE_URL    = 'https://api2.eventfinity.co'
-
-    // #READ_WRITE_GOODBURGER CpaFZ2lS6HmSHoBbkmxJIg2kT6s01w3R2Mcsu03u
-    // #READ_ONLY_GOODBURGER s2tbYsgFUTyOz4NluIu8OupD9leOqZMWvP31Veat
-
      // Convert the image data to a Blob object
      const contentType = 'image/png';
      const blob = base64StringToBlob(b64, contentType);
-    
-    //  generateDownloadLink(blob)
-
-     //const filename = path.basename(imagePath);
-
-     //console.log("SENDING PHOTO TO STREAM: " + filename);
 
      // Create a new FormData instance
      const formData = new FormData();
@@ -120,13 +83,13 @@ function Home() {
      const requestOptions = {
          method: 'POST',
          headers: {
-             'Authorization': 'Bearer CpaFZ2lS6HmSHoBbkmxJIg2kT6s01w3R2Mcsu03u'
+             'Authorization': `Bearer ${import.meta.env.VITE_PUBLIC_READ_ONLY_TOKEN}`
          },
          body: formData
      };
 
     // Perform the POST request
-      fetch("https://api2.eventfinity.co/api/v1/public/events/107551/photostreams/64731/photos", requestOptions)
+      fetch(`${import.meta.env.VITE_PUBLIC_API_BASE_URL}/api/v1/public/events/${import.meta.env.VITE_PUBLIC_EVENT_ID}/photostreams/${import.meta.env.VITE_PUBLIC_PHOTOSTREAM_ID}/photos`, requestOptions)
           .then(response => response.text())
           .then(result => {
               //console.log(result);
@@ -135,11 +98,12 @@ function Home() {
               //event.sender.send('form-submission-successful', result);
           })
           .catch(error => {
-              //console.log('error', error);
+              console.log('error', error);
               //event.sender.send('form-submission-failed', error);
           });
   }
   const restart  = () =>{
+    // SHOW FINAL IMAGE AND RESTART
     const myTimeout = setTimeout(() => {
       const canvas  = document.querySelector('canvas')
       const context = canvas.getContext('2d');
