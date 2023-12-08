889d49b87664a23f07cc4d34ace4ed1f8f26c248
Douglas Moreira <dougmoreira@Douglass-MBP.lan>
2023-12-08 14:36:24 -0500

    frame test v2 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
 
   const getVideo = () =>{
     navigator.mediaDevices
-      .getUserMedia({ video:{width: 1000, height:420}}).then( stream =>{
+      .getUserMedia({ video:{width: 900, height:420}}).then( stream =>{
         let video =  videoRef.current;
         video.srcObject = stream;
         video.play();
